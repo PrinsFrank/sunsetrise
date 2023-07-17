2802a924644238c475c207c116b5107714436a49
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2023-07-17 22:22:52 +0300

    Add NightPeriodType

---
+++ src/Period/NightPeriodType.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace PrinsFrank\SunSetRise\Period;
+
+enum NightPeriodType
+{
+    /** Defined as when the sun is between 0 and 6 degrees below the horizon */
+    case Civil_Twilight;
+    /** Defined as when the sun is between 6 and 12 degrees below the horizon */
+    case Nautical_Twilight;
+    /** Defined as when the sun is between 12 and 18 degrees below the horizon */
+    case Astronomical_Twilight;
+    /** Defined as when the sun is more than 18 degrees below the horizon */
+    case Night;
+}
