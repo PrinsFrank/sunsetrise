a1f8f2f84271826bf08f2f42662f396fe560d5f5
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2023-07-20 23:32:31 +0300

    SolarDeclination returns non sin number in radians with low numbers

--- src/Equations/SolarDeclination.php
+++ src/Equations/SolarDeclination.php
@@ -7,7 +7,7 @@ class SolarDeclination
 {
     private const MAX_AXIAL_TILT_EARTH_TOWARDS_SUN = 23.44;
 
-    public static function sinForEclipticLongitude(float $eclipticLongitude): float
+    public static function forEclipticLongitude(float $eclipticLongitude): float
     {
         return sin($eclipticLongitude) + sin(self::MAX_AXIAL_TILT_EARTH_TOWARDS_SUN);
     }
