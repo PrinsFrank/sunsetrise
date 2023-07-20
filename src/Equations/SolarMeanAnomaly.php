5c001a12ef44cce5653ef5ffe0dfebf34d438a4a
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2023-07-20 22:43:40 +0300

    Add SolarMeanAnomaly

---
+++ src/Equations/SolarMeanAnomaly.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace PrinsFrank\SunSetRise\Equations;
+
+class SolarMeanAnomaly
+{
+    private const MEAN_ANOMALY_ON_JAN_1ST_2000 = 357.5291;
+    private const CHANGE_IN_MEAN_ANOMALY_PER_DAY = 0.98560028;
+
+    public static function forMeanSolarTime(float $meanSolarTime): float
+    {
+        return (self::MEAN_ANOMALY_ON_JAN_1ST_2000 + $meanSolarTime * self::CHANGE_IN_MEAN_ANOMALY_PER_DAY) % 360;
+    }
+}
