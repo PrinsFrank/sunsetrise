fec980edd56be8ba0ffee35a85774880d27a42fc
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2023-07-21 21:50:00 +0300

    Return asin for SolarDeclination

--- src/Equations/SolarDeclination.php
+++ src/Equations/SolarDeclination.php
@@ -7,9 +7,8 @@ class SolarDeclination
 {
     private const MAX_AXIAL_TILT_EARTH_TOWARDS_SUN = 23.44;
 
-    /** The formula returns sin δ, but In radians, δ ≈ sin δ*/
     public static function forEclipticLongitude(float $eclipticLongitude): float
     {
-        return sin($eclipticLongitude) + sin(self::MAX_AXIAL_TILT_EARTH_TOWARDS_SUN);
+        return asin(sin($eclipticLongitude) + sin(self::MAX_AXIAL_TILT_EARTH_TOWARDS_SUN));
     }
 }
